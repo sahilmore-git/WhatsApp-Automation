8ae1b26e45804cbccca0ff4ed1219a7fbb01ecaf
model64321 <model64321@gmail.com>
2020-06-06 16:10:41 +0530

    Commit-Changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # WhatsApp-Automation
+
+##PATH :
+
+Set path for your chromedriver.exe and numbers.xlsx in "app.py"
+
+##STEPS :
+
+1) Run Command "pip install -r requirments.txt".
+2) Install AutoIt for sending attachments.
+3) Run the Script "python app.py". 
---
+++ app.py
@@ -0,0 +1,133 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+try:
+    import autoit
+except ModuleNotFoundError:
+    pass
+import time
+import datetime
+import os
+import openpyxl
+import pyfiglet 
+
+choice = None
+medianame = None
+docChoice = None
+doc_filename = None
+
+print("\n\nDeveloped By :")
+print(pyfiglet.figlet_format("mOdEL^", font = "slant" ))
+
+unsaved_Contacts=[]
+
+path_excel_file="c:/your/path/to/numbers.xlsx"
+wb = openpyxl.load_workbook(path_excel_file)
+sheet = wb['Sheet1']
+col = sheet['A']
+
+for cell in col:
+        time.sleep(1)
+        inp = '"' + str(cell.value) + '"'
+        unsaved_Contacts.append(inp)
+print(unsaved_Contacts)
+
+choice = input("Would you like to send attachment(yes/no): ")
+if(choice == "yes"):
+        # Note the media files should be present in the Media Folder
+    medianame = input("Enter the Media file name you want to send: ")
+docChoice = input("Would you file to send a Document file(yes/no): ")
+
+if(docChoice == "yes"):
+        # Note the document file should be present in the Document Folder
+    doc_filename = input("Enter the Document file name you want to send: ")
+
+
+
+def send_files():
+    global doc_filename
+    # Attachment Drop Down Menu
+    clipButton = driver.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
+    clipButton.click()
+    time.sleep(3)
+
+    # To send a Document(PDF, Word file, PPT)
+    docButton = driver.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[3]/button')
+    docButton.click()
+    time.sleep(3)
+
+    docPath = os.getcwd() + "\\Documents\\" + doc_filename
+
+    autoit.control_focus("Open", "Edit1")
+    autoit.control_set_text("Open", "Edit1", (docPath))
+    autoit.control_click("Open", "Button1")
+    time.sleep(5)  
+    whatsapp_send_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
+    whatsapp_send_button.click()
+
+
+def send_attachment():
+    global medianame
+    # Attachment Drop Down Menu
+    clipButton = driver.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
+    clipButton.click()
+    time.sleep(3)
+
+    # To send Videos and Images.
+    mediaButton = driver.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[1]/button')
+    mediaButton.click()
+    time.sleep(3)
+    image_path = os.getcwd() + "\\Media\\" + medianame
+    autoit.control_focus("Open", "Edit1")
+    autoit.control_set_text("Open", "Edit1", image_path)
+    autoit.control_click("Open", "Button1")
+
+    time.sleep(3)  
+    whatsapp_send_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
+    whatsapp_send_button.click()
+            
+try:
+    driver = webdriver.Chrome("c:/your/path/to/chromedriver.exe")
+
+    def send_unsaved_contact_message():
+        message = input('Enter your message : ')
+        # message = "" #HardCode it here.
+        try:
+            time.sleep(5)
+            input_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+            for ch in message:
+                if ch == "\n":
+                    ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.BACKSPACE).perform()
+                else:
+                    input_box.send_keys(ch)
+            input_box.send_keys(Keys.ENTER)
+            print("Message sent successfuly")
+        except NoSuchElementException:
+            print("Failed to send message no Element.")
+            return
+
+    for i in unsaved_Contacts:
+            link = "https://web.whatsapp.com/send?phone={}&text&source&data&app_absent".format(i)
+            driver.get(link)
+            print("Sending message to", i)
+            send_unsaved_contact_message()
+            time.sleep(2)
+            if(choice == "yes"):
+                try:
+                    send_attachment()
+                except:
+                    print('Attachment not sent.')
+            if(docChoice == "yes"):
+                try:
+                    send_files()
+                except:
+                    print('Files not sent')
+                    time.sleep(5)
+            time.sleep(2)
+except Exception as e:
+    print(e)
